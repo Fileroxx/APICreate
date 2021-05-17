a291d968f5498f5c2acf344587988dd08331e5e1
Igor Fernandes <igor_cesari@hotmail.com>
2021-05-17 11:22:33 -0300

    API-stale-while-revalidate

--- api-create/pages/api/tempo.js
+++ api-create/pages/api/tempo.js
@@ -1,5 +1,8 @@
-function tempo(request, response) {
+async function tempo(request, response) {
     const dynamicDate = new Date();
+    
+    respose.setHeader('Cache-control', 's-maxage=10', 'stale-while-revalidate');
+
 
     response.json({
         date: dynamicDate.toGMTString()
